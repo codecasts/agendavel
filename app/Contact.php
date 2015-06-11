a95e727ea049a61d85d9d7e214592167d410689c
Diego Hernandes <diego@hernandev.com>
2015-06-11 00:34:00 -0300

    campos "preenchiveis" no model Contact

--- app/Contact.php
+++ app/Contact.php
@@ -8,6 +8,8 @@ class Contact extends Model
 {
     protected $table = 'contacts';
 
+    protected $fillable = ['name', 'comments'];
+
     public function numbers()
     {
         return $this->hasMany(Number::class, 'contact_id');
