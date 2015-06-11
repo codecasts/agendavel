ab1181f12bf8bf3ddccbc8ad9ac6903dc6397b0b
Diego Hernandes <diego@hernandev.com>
2015-06-11 00:49:45 -0300

    factory para Agendavel\User

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,11 +11,18 @@
 |
 */
 
-$factory->define(App\User::class, function ($faker) {
+$factory->define(Agendavel\User::class, function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
-        'password' => str_random(10),
+        'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Agendavel\Contact::class, function($faker) {
+    return [
+        'name'  =>  $faker->name,
+        'comments'  =>  str_random(20),
     ];
 });
