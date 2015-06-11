05426a221e55c4b2c89f7d3ce7db0ba861d28018
Diego Hernandes <diego@hernandev.com>
2015-06-11 00:31:39 -0300

    relacionamento numbers() adicionado ao model Contact

--- app/Contact.php
+++ app/Contact.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $table = 'contacts';
+
+    public function numbers()
+    {
+        return $this->hasMany(Number::class, 'contact_id');
+    }
 }
