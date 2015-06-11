d7f0fa0b65cd9992727a457a7d70378bc6b8d5cf
Diego Hernandes <diego@hernandev.com>
2015-06-10 23:03:53 -0300

    namespace setado com 'php artisan app:name Agendavel'

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Agendavel\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Agendavel\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class PasswordController extends Controller
--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model'  => App\User::class,
+        'model'  => Agendavel\User::class,
         'key' => '',
         'secret' => '',
     ],
